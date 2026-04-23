08b66adde41079cb9cb3d955f94ebe73fa269ce6
WEI MING <55676193+wayne30691@users.noreply.github.com>
2026-04-23 22:00:15 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -84,7 +84,7 @@ def _file_uploader_with_memory(label, *args, **kwargs):
 st.file_uploader = _file_uploader_with_memory
 
 # Streamlit app title
-st.title("📊 T2 WS Transformations (Test Environment)")
+st.title("📊 T2 WS Transformations")
 st.write("Upload an Excel file and choose the transformation format.")
 
 
